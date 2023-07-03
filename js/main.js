8bb72f5e5d4fcd97ed1412cadf161e0e5c6d7f13
NikolGraminova <nbackup619@gmail.com>
2023-07-03 14:50:58 +0200

    Added form and variables

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero (con i prompt() in questo caso).
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo e questo richiederà un minimo di ricerca.).
+Il risultato andrà visualizzato prima in console e poi possibilmente in un apposito elemento <p> del mio codice HTML.
+BONUS:
+Il programma dovrà mostrare una form da compilare con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina.*/
+
+
+
+
+function submitForm(event) {
+    
+    event.preventDefault();
+    console.log("test form")
+
+
+    let userInfo = document.getElementById("inputInfo").value;
+    let userKilometers = document.getElementById("inputKilometers").value;
+    let userTicket = document.getElementById("inputTicket").value;
+
+}
